5893771622aedd5e8f7d4ba45d2fa32d18df5755
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-11-07 22:03:18 -0300

    routing for nginx fixed

--- src/routes.ts
+++ src/routes.ts
@@ -4,87 +4,87 @@ export const Routes = [
     // food local
     {
         method: "get",
-        route: "/api/v1/food/local",
+        route: "/food/local",
         controller: MainController,
         action: "foodLocalAll"
     }, 
     {
         method: "get",
-        route: "/api/v1/food/local/:id",
+        route: "/food/local/:id",
         controller: MainController,
         action: "foodLocalOne"
     }, 
     {
         method: "post",
-        route: "/api/v1/food/local/",
+        route: "/food/local/",
         controller: MainController,
         action: "foodLocalSaveLocal"
     }, 
     {
         method: "delete",
-        route: "/api/v1/food/local/:foodLocalId",
+        route: "/food/local/:foodLocalId",
         controller: MainController,
         action: "foodLocalRemove"
     },
     // user rates food
     {
         method: "get",
-        route: "/api/v1/food/ratings",
+        route: "/food/ratings",
         controller: MainController,
         action: "userRatesFoodAll"
     }, 
     {
         method: "get",
-        route: "/api/v1/food/ratings/byuserandfood/:userId/:foodLocalId",
+        route: "/food/ratings/byuserandfood/:userId/:foodLocalId",
         controller: MainController,
         action: "userRatesFoodOne"
     }, 
     {
         method: "get",
-        route: "/api/v1/food/ratings/byuser/:userId",
+        route: "/food/ratings/byuser/:userId",
         controller: MainController,
         action: "userRatesFoodAllByUser"
     }, 
     {
         method: "post",
-        route: "/api/v1/food/ratings",
+        route: "/food/ratings",
         controller: MainController,
         action: "userRatesFoodSave"
     }, 
     {
         method: "delete",
-        route: "/api/v1/food/ratings/byuserandfood/:foodLocalId/:userId",
+        route: "/food/ratings/byuserandfood/:foodLocalId/:userId",
         controller: MainController,
         action: "userRatesFoodRemove"
     },
     {
         method: "delete",
-        route: "/api/v1/food/ratings/byuser/:userId",
+        route: "/food/ratings/byuser/:userId",
         controller: MainController,
         action: "userRatesFoodRemoveByUser"
     },
     {
         method: "delete",
-        route: "/api/v1/food/ratings/byfood/:foodLocalId",
+        route: "/food/ratings/byfood/:foodLocalId",
         controller: MainController,
         action: "userRatesFoodRemoveByFood"
     },
     {
         method: "get",
-        route: "/api/v1/food/ratings/byfood/:foodLocalId",
+        route: "/food/ratings/byfood/:foodLocalId",
         controller: MainController,
         action: "userRatesFoodRatingsByFood"
     }, 
     // open food facts
     {
         method: "get",
-        route: "/api/v1/food/external/:id",
+        route: "/food/external/:id",
         controller: MainController,
         action: "foodExternalOne"
     }, 
     {
         method: "get",
-        route: "/api/v1/food/allergens",
+        route: "/food/allergens",
         controller: MainController,
         action: "allergensAll"
     }, 
